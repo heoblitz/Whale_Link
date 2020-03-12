ad437b3027db7782beb1152662e7b040c7a66564
wone2287 <wone2287@gmail.com>
2020-03-12 18:15:12 +0900

    add storage permission in manifest.json

--- background.js
+++ background.js
@@ -1,21 +1,11 @@
-/*
-var host = "http://tpb.pirateparty.org.uk";
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
-    },
-    {
-        urls: [
-            "*://piratebay.se/*",
-            "*://www.piratebay.se/*"
-        ],
-        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
-    },
-    ["blocking"]
-);
+chrome.runtime.onInstalled.addListener(function() {
+    /*
+    chrome.contextMenus.create({
+      "id": "sampleContextMenu",
+      "title": "Sample Context Menu",
+      "contexts": ["selection"]
+    });
+    */
 
-
-        "webRequest",
-        "webRequestBlocking",
-        "*://*.naver.com/",
-*/
+    Console.log("hello");
+  });
