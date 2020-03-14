9904025dca74f3242df1c6b52791ea0f5748e83c
wone2287 <wone2287@gmail.com>
2020-03-14 11:33:34 +0900

    edit save method

--- background.js
+++ background.js
@@ -1,12 +1,32 @@
-var theValue = ["a", "b"];
+/*var urlList = {
+  "부동산" : ["https://new.land.naver.com/search", "sk"],
+  "쇼핑" : ["https://search.shopping.naver.com/search/all.nhn", "query"],
+  "지도" : ["https://map.naver.com/v5/search/", ""],
+  "사전" : ["https://dict.naver.com/search.nhn", "query"],
+  "지식인" : ["https://kin.naver.com/search/list.nhn", "query"],
+  "포스트" : ["https://post.naver.com/search/post.nhn", "keyword"],
+  "구지도" : ["https://map.naver.com/?sm=hty", "query"],
+  "tv" : ["https://tv.naver.com/search/clip", "query"],
+  "검색" : ["https://search.naver.com/search.naver", "query"],
+  "플레이스" : ["https://store.naver.com/restaurants/list", "query"],
+  "웹툰" : ["https://comic.naver.com/search.nhn", "keyword"],
+  "what" : "what",
+};*/
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.set({"저장": theValue}, function () {
-    // 저장 완료
+// test code
+  save("hi", "world");
 });
 
-chrome.storage.local.get("저장", function (items) {
-  alert(items.저장);
-  // items: 저장한 객체의 key/value
-});
-});
+function save(key, value) {
+  chrome.storage.local.set({"hi": "world"}, function () {
+    // 저장 완료
+    get("hi");
+  });
+}
+
+function get(key) {
+  chrome.storage.local.get(["hi"], function (result) {
+    alert(result[key]);
+  });
+}
