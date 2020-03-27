8eb5465b06c8b997599ab4f2b53a6c00a1c7026d
wone2287 <wone2287@gmail.com>
2020-03-27 16:31:36 +0900

    Access to URL using Storage api

--- build/naverlink.js
+++ build/naverlink.js
@@ -2,18 +2,86 @@ document.addEventListener('keydown', userKey);
 var send = document.getElementById('send');
 send.onclick = userClick;
 
-
+// event
  
 function userClick(){ // user button 클릭 감지.
     AccessToUrl();
 }
-
 
 function userKey(){ // user enter 입력 감지.
     if(window.event.keyCode == 13){
         AccessToUrl();
     }
 }
+
+function AccessToUrl() {
+
+    let Posts = AccessDB();
+    let userInput = document.getElementById("query").value;
+    let url;
+
+    userInputList = userInput.split(' ');
+
+    if (Posts[userInputList[0]] == undefined) { 
+        alert("지정되지 않은 키입니다.")
+    }
+
+    else {
+        url = ParseCommand(Posts, userInputList);
+    }
+
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab){
+        chrome.tabs.update(tab.id, {url: url});
+    });
+}
+
+function AccessDB() {
+    let Posts;
+
+    if( localStorage.getItem("Posts") == null ) {
+        alert("DB 오류");
+    }
+
+    else {
+        Posts = JSON.parse(localStorage.getItem("Posts"));
+    }
+
+    return Posts;
+}
+
+function ParseCommand(Posts, userInputList) {
+    let url = Posts[userInputList[0]]["url"];
+    var query = "";
+
+    for(let i=1;i<userInputList.length;i++){ // 사용자가 공백을 주고 입력한 값 for 문 돌림
+        query += encodeURI(userInputList[i]); 
+
+        if(i != userInputList.length-1){ // 문자열 끝이 아니라면 공백 추가
+            query += ' ';
+        }
+    }
+
+    url = url.replace("{query}", query);
+
+    return url;
+}
+
+
+/*
+var urlList = [
+    ["부동산", "https://new.land.naver.com/search?sk={query}}", "https://search.naver.com/favicon.ico"],
+    ["쇼핑", "https://search.shopping.naver.com/search/all.nhn?query={query}", "https://land.naver.com/favicon.ico"],
+    ["지도", "https://map.naver.com/v5/search/{query}", "https://ssl.pstatic.net/imgshopping/cnsv/p/im/home/favicon.ico"],
+    ["사전", "https://dict.naver.com/search.nhn?query={query}", "https://dict.naver.com/favicon.ico"],
+    ["지식인", "https://kin.naver.com/search/list.nhn?query={query}", "https://kin.naver.com/favicon.ico"],
+    ["포스트", "https://post.naver.com/search/post.nhn?keyword={query}", "https://post.naver.com/favicon.ico"],
+    ["네이버 TV", "https://tv.naver.com/search/clip?query={query}", "https://tv.naver.com/favicon.ico"],
+    ["검색", "https://search.naver.com/search.naver?query={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico"],
+    ["플레이스", "https://store.naver.com/restaurants/list?query={query}", "https://store.naver.com/favicon.ico"],
+    ["웹툰", "https://comic.naver.com/search.nhn?keyword={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico"],
+];
+// 이중 배열로 수정하자.
+
 
 function AccessToUrl() {
     var userInput = document.getElementById("query").value;
@@ -48,25 +116,5 @@ function AccessToUrl() {
         chrome.tabs.update(tab.id, {url: custumUrl});
     });
 }
-
-function ParseCommand() {
-    var userInput = document.getElementById("query").value;
-
-}
-
-
-/*
-var urlList = [
-    ["부동산", "https://new.land.naver.com/search?sk={query}}", "https://search.naver.com/favicon.ico"],
-    ["쇼핑", "https://search.shopping.naver.com/search/all.nhn?query={query}", "https://land.naver.com/favicon.ico"],
-    ["지도", "https://map.naver.com/v5/search/{query}", "https://ssl.pstatic.net/imgshopping/cnsv/p/im/home/favicon.ico"],
-    ["사전", "https://dict.naver.com/search.nhn?query={query}", "https://dict.naver.com/favicon.ico"],
-    ["지식인", "https://kin.naver.com/search/list.nhn?query={query}", "https://kin.naver.com/favicon.ico"],
-    ["포스트", "https://post.naver.com/search/post.nhn?keyword={query}", "https://post.naver.com/favicon.ico"],
-    ["네이버 TV", "https://tv.naver.com/search/clip?query={query}", "https://tv.naver.com/favicon.ico"],
-    ["검색", "https://search.naver.com/search.naver?query={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico"],
-    ["플레이스", "https://store.naver.com/restaurants/list?query={query}", "https://store.naver.com/favicon.ico"],
-    ["웹툰", "https://comic.naver.com/search.nhn?keyword={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico"],
-];
-// 이중 배열로 수정하자.
 */
+
--- build/setting.js
+++ build/setting.js
@@ -1,16 +1,15 @@
 var urlList = [
-    ["부동산", "https://new.land.naver.com/search?sk={query}}", "https://search.naver.com/favicon.ico", "네이버 검색"],
-    ["쇼핑", "https://search.shopping.naver.com/search/all.nhn?query={query}", "https://land.naver.com/favicon.ico", "네이버 부동산"],
-    ["지도", "https://map.naver.com/v5/search/{query}", "https://ssl.pstatic.net/imgshopping/cnsv/p/im/home/favicon.ico", "네이버 쇼핑"],
-    ["사전", "https://dict.naver.com/search.nhn?query={query}", "https://dict.naver.com/favicon.ico", "네이버 지도"],
-    ["지식인", "https://kin.naver.com/search/list.nhn?query={query}", "https://kin.naver.com/favicon.ico", "네이버 사전"],
-    ["포스트", "https://post.naver.com/search/post.nhn?keyword={query}", "https://post.naver.com/favicon.ico", "네이버 지식인"],
-    ["네이버 TV", "https://tv.naver.com/search/clip?query={query}", "https://tv.naver.com/favicon.ico", "네이버 포스트"],
-    ["검색", "https://search.naver.com/search.naver?query={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico", "네이버 TV"],
-    ["플레이스", "https://store.naver.com/restaurants/list?query={query}", "https://store.naver.com/favicon.ico", "네이버 플레이스"],
-    ["웹툰", "https://comic.naver.com/search.nhn?keyword={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico", "네이버 웹툰"],
+    ["네이버 부동산", "https://new.land.naver.com/search?sk={query}}", "https://land.naver.com/favicon.ico", "부동산"],
+    ["네이버 쇼핑", "https://search.shopping.naver.com/search/all.nhn?query={query}", "https://ssl.pstatic.net/imgshopping/cnsv/p/im/home/favicon.ico", "쇼핑"],
+    ["네이버 지도", "https://map.naver.com/v5/search/{query}", "https://map.naver.com/v5/assets/icon/favicon-32x32.png", "지도"],
+    ["네이버 사전", "https://dict.naver.com/search.nhn?query={query}", "https://dict.naver.com/favicon.ico", "사전"],
+    ["네이버 지식인", "https://kin.naver.com/search/list.nhn?query={query}", "https://kin.naver.com/favicon.ico", "지식인"],
+    ["네이버 포스트", "https://post.naver.com/search/post.nhn?keyword={query}", "https://post.naver.com/favicon.ico", "포스트"],
+    ["네이버 TV", "https://tv.naver.com/search/clip?query={query}", "https://tv.naver.com/favicon.ico", "tv"],
+    ["네이버 검색", "https://search.naver.com/search.naver?query={query}", "https://www.naver.com/favicon.ico", "검색"],
+    ["네이버 플레이스", "https://store.naver.com/restaurants/list?query={query}", "https://store.naver.com/favicon.ico", "플레이스"],
+    ["네이버 웹툰", "https://comic.naver.com/search.nhn?keyword={query}", "https://ssl.pstatic.net/static/comic/favicon/webtoon_favicon_32x32.ico", "웹툰"],
 ];
-
 
 var DataDto = function (serviceName, url, faviconUrl, hotKey) {
     this.serviceName = serviceName;
@@ -31,7 +30,7 @@ var Posts = new Map();
 if (localStorage.getItem("Posts") == null) {
     for (let i = 0; i < urlList.length; i++) {
         let data = new DataDto(urlList[i][0], urlList[i][1], urlList[i][2], urlList[i][3]);
-        Posts.set(urlList[i][0], data);
+        Posts.set(urlList[i][3], data);
     }
 
     Posts = mapToObj(Posts);
@@ -44,9 +43,6 @@ if (localStorage.getItem("Posts") == null) {
 
 else {
     Posts = JSON.parse(localStorage.getItem("Posts"));
-
-    alert("Posts is Getting now");
-
     renderPosts();
 }
 
@@ -66,7 +62,7 @@ function createHtmlList(data) {
                         "<h3 class=\"query-name\">" + data["serviceName"] + "</h3>" +
                         "<span class=\"shortcut\">단축키</span>" +
                         "<input type=\"text\" value=\"" + data["hotKey"] + "\" class=\"contents-hotkey\">" +
-                        "</li>";
+                    "</li>";
 
     return htmlCode;
 }
